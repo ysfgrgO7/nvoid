0909898e27236cf666cdd9cebca08efb13e05158
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-05-27 15:56:05 +0300

    fixing config

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,8 +1,10 @@
-nvoid.lsp.diagnostics.virtual_text = false
+vim.diagnostic.config({
+  virtual_text = false
+})
 nvoid.lsp.format_on_save = false
 nvoid.lsp.document_highlight = true
 nvoid.lsp.installer.setup.ensure_installed = {
-  "sumneko_lua",
+  "lua_ls",
   "bashls",
   "pyright",
 }
