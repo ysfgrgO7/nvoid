756269138c5f7f24842ec31d635d638c5a9693f4
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-06-28 22:55:41 +0300

    cleaning

--- lua/ysf/config/cokeline.lua
+++ lua/ysf/config/cokeline.lua
@@ -1,14 +1,102 @@
 local get_hex = require('cokeline/utils').get_hex
 
-require('cokeline').setup({
-  show_if_buffers_are_at_least = 2,
+local yellow = vim.g.terminal_color_3
 
-  buffers = {
-    new_buffers_position = 'next',
+local comments_fg = get_hex('Comment', 'fg')
+local errors_fg = get_hex('DiagnosticError', 'fg')
+
+local components = {
+  separator = {
+    text = ' ',
+    bg = get_hex('TablineFill', 'bg'),
+    truncation = { priority = 1 },
   },
 
-  rendering = {
-    max_buffer_width = 30,
+  space = {
+    text = ' ',
+    truncation = { priority = 1 },
+  },
+
+
+  devicon = {
+    text = function(buffer)
+      return buffer.devicon.icon
+    end,
+    fg = function(buffer)
+      return buffer.devicon.color
+    end,
+    truncation = { priority = 1 },
+  },
+
+  index = {
+    text = function(buffer)
+      return buffer.index .. ': '
+      -- return buffer.index .. '. '
+    end,
+    fg = function(buffer)
+      return
+          (buffer.diagnostics.errors ~= 0 and errors_fg)
+          or nil
+    end,
+    truncation = { priority = 1 },
+  },
+
+  unique_prefix = {
+    text = function(buffer)
+      return buffer.unique_prefix
+    end,
+    fg = comments_fg,
+    style = 'italic',
+    truncation = {
+      priority = 3,
+      direction = 'left',
+    },
+  },
+
+  filename = {
+    text = function(buffer)
+      return buffer.filename
+    end,
+    fg = function(buffer)
+      return
+          (buffer.diagnostics.errors ~= 0 and errors_fg)
+          or nil
+    end,
+    style = function(buffer)
+      return
+          ((buffer.is_focused and buffer.diagnostics.errors ~= 0)
+            and 'bold,underline')
+          or (buffer.is_focused and 'bold')
+          or (buffer.diagnostics.errors ~= 0 and 'underline')
+          or nil
+    end,
+    truncation = {
+      priority = 2,
+      direction = 'left',
+    },
+  },
+
+  close_or_unsaved = {
+    text = function(buffer)
+      -- return buffer.is_modified and '●' or ''
+      return buffer.is_modified and '' or ''
+    end,
+    fg = function(buffer)
+      return buffer.is_modified and '#00bb00' or nil
+    end,
+    delete_buffer_on_left_click = false,
+    truncation = { priority = 1 },
+  },
+}
+
+require('cokeline').setup({
+  show_if_buffers_are_at_least = 1,
+
+  buffers = {
+    filter_valid = function(buffer) return buffer.type ~= 'terminal' end,
+    filter_visible = function(buffer) return buffer.type ~= 'terminal' end,
+    focus_on_delete = 'prev',
+    new_buffers_position = 'next',
   },
 
   default_hl = {
@@ -24,5 +112,27 @@ require('cokeline').setup({
           and get_hex('Coke', 'bg')
           or get_hex('Cokeun', 'bg')
     end,
+  },
+
+  sidebar = {
+    filetype = 'NvimTree',
+    components = {
+      {
+        text = "",
+        fg = yellow,
+        bg = get_hex('NvimTreeNormal', 'bg'),
+        style = 'bold',
+      },
+    }
+  },
+
+  components = {
+    components.space,
+    components.devicon,
+    components.index,
+    components.unique_prefix,
+    components.filename,
+    components.close_or_unsaved,
+    components.space,
   },
 })
--- lua/ysf/hl.lua
+++ lua/ysf/hl.lua
@@ -1,6 +1,22 @@
 local M = {}
 M.add = {
   Coke = { bg = "black", fg = "white" },
-  Cokeun = { bg = "darker_black", fg = "grey" }
+  Cokeun = { bg = "darker_black", fg = "grey" },
+  BufferVisible = { fg = 'fg', bg = 'darker_black' },
+  BufferVisibleIndex = { fg = 'blue', bg = 'darker_black' },
+  BufferVisibleMod = { fg = 'yellow', bg = 'darker_black' },
+  BufferVisibleSign = { fg = 'blue', bg = 'darker_black' },
+  BufferVisibleTarget = { fg = 'red', bg = 'darker_black', bold = true, },
+  BufferInactive = { fg = 'grey_fg2', bg = 'darker_black' },
+  BufferInactiveIndex = { fg = 'grey_fg2', bg = 'darker_black' },
+  BufferInactiveMod = { fg = '#a58354', bg = 'darker_black' },
+  BufferInactiveSign = { fg = 'blue', bg = 'darker_black' },
+  BufferInactiveTarget = { fg = 'red', bg = 'darker_black', bold = true, },
+  BufferTabpage = { fg = 'blue', bg = 'darker_black' },
+  BufferTabpages = { fg = 'NONE', bg = 'darker_black' },
+
+  TabLine = { fg = 'one_bg2', bg = 'darker_black' },
+  TabLineSel = { fg = 'black', bg = 'blue' },
+  TabLineFill = { fg = 'black', bg = 'black' },
 }
 return M
