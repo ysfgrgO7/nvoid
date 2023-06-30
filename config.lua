3c5518cb95038356c6f6b96f56c7f42533fe702f
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-07-01 02:00:20 +0300

    Cool Cmp

--- config.lua
+++ config.lua
@@ -3,5 +3,5 @@ nvoid.ui.colorscheme = "tokyonight"
 nvoid.ui.statusline.config = "%!v:lua.reload('ysf.statusline').run()"
 nvoid.log.level = "warn"
 nvoid.ui.hl_add = reload("ysf.hl").add
--- nvoid.ui.hl_override = reload("ysf.hl").change
+nvoid.ui.hl_override = reload("ysf.hl").change
 reload("ysf.builtins")
--- lua/ysf/builtins.lua
+++ lua/ysf/builtins.lua
@@ -5,6 +5,15 @@ nvoid.keys.normal_mode["<C-b>"] = ":JABSOpen<CR>"
 nvoid.keys.normal_mode["<S-x>"] = ":BufferKill<CR>"
 nvoid.keys.normal_mode["<TAB>"] = ":BufferNext<CR>"
 nvoid.keys.normal_mode["<S-TAB>"] = ":BufferPrev<CR>"
+--## CMP
+nvoid.builtin.cmp.formatting.kind_icons = reload("ysf.kinds")
+nvoid.builtin.cmp.window.completion.border = { "", "", "", "", "", "", "", "" }
+nvoid.builtin.cmp.window.completion.winhighlight = "Normal:Pmenu,CursorLine:PmenuSel,FloatBorder:FloatBorder,Search:None"
+nvoid.builtin.cmp.window.completion.side_padding = 0
+nvoid.builtin.cmp.window.documentation.border = { "", "", "", "", "", "", "", "" }
+nvoid.builtin.cmp.window.documentation.winhighlight =
+"Normal:Pmenu,CursorLine:PmenuSel,FloatBorder:FloatBorder,Search:None"
+nvoid.builtin.cmp.window.documentation.side_padding = 0
 --## Which Key
 nvoid.builtin.which_key.setup.layout.align = "center"
 nvoid.builtin.which_key.mappings["s"] = {
