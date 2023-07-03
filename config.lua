97e27c036b43fdb4a67158cdf6e2d772a7ce9eaa
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-07-03 14:40:29 +0300

    formatting

--- config.lua
+++ config.lua
@@ -8,12 +8,14 @@ reload "ysf.builtins.cmp"
 reload "ysf.builtins.wk"
 reload "ysf.builtins.nt"
 reload "ysf.builtins.ts"
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+vim.cmd [[
+  aunmenu PopUp.How-to\ disable\ mouse
+  aunmenu PopUp.-1-
+]]
 local common_opts = { force = true }
 for _, cmds in pairs(reload "ysf.cmds") do
   local opts = vim.tbl_deep_extend("force", common_opts, cmds.opts or {})
   vim.api.nvim_create_user_command(cmds.name, cmds.fn, opts)
 end
-vim.cmd [[
-  aunmenu PopUp.How-to\ disable\ mouse
-  aunmenu PopUp.-1-
-]]
