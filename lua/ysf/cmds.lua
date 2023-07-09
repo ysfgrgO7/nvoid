48d0f89d29a1282d6ab1e132d835efc13f71f48f
ysfgrgO7 <ysfgrgO7@gmail.com>
2023-07-10 01:33:37 +0300

    better kill

--- lua/ysf/cmds.lua
+++ lua/ysf/cmds.lua
@@ -1,19 +1,16 @@
+local api = vim.api
 local bufilter = function()
   local bufs = vim.t.bufs or nil
-
   if not bufs then
     return {}
   end
-
   for i = #bufs, 1, -1 do
     if not vim.api.nvim_buf_is_valid(bufs[i]) and vim.bo[bufs[i]].buflisted then
       table.remove(bufs, i)
     end
   end
-
   return bufs
 end
-
 local getBufIndex = function(bufnr)
   for i, value in ipairs(vim.t.bufs) do
     if value == bufnr then
@@ -26,10 +23,10 @@ return {
   {
     name = "BufferToggle",
     fn = function()
-      if #vim.fn.getbufinfo { buflisted = 1 } >= 2 or #vim.api.nvim_list_tabpages() >= 2 then
+      if #vim.fn.getbufinfo { buflisted = 1 } >= 2 or #api.nvim_list_tabpages() >= 2 then
         vim.opt.showtabline = 2
         vim.opt.tabline = "%!v:lua.require('ysf.ui.bufferline.modules').run()"
-      elseif #vim.fn.getbufinfo { buflisted = 1 } >= 0 or #vim.api.nvim_list_tabpages() >= 1 then
+      elseif #vim.fn.getbufinfo { buflisted = 1 } >= 0 or #api.nvim_list_tabpages() >= 1 then
         vim.opt.tabline = ""
         vim.opt.showtabline = 0
         vim.opt.tabline = ""
@@ -39,7 +36,7 @@ return {
   {
     name = "BufferKill",
     fn = function()
-      vim.cmd "bd"
+      require("ysf.ui.bufferline").close_buffer()
       vim.cmd "BufferToggle"
     end,
   },
@@ -47,13 +44,11 @@ return {
     name = "BufferNext",
     fn = function()
       local bufs = bufilter() or {}
-      local curbufIndex = getBufIndex(vim.api.nvim_get_current_buf())
-
+      local curbufIndex = getBufIndex(api.nvim_get_current_buf())
       if not curbufIndex then
         vim.cmd("b" .. vim.t.bufs[1])
         return
       end
-
       vim.cmd(curbufIndex == #bufs and "b" .. bufs[1] or "b" .. bufs[curbufIndex + 1])
     end,
   },
@@ -61,13 +56,11 @@ return {
     name = "BufferPrev",
     fn = function()
       local bufs = bufilter() or {}
-      local curbufIndex = getBufIndex(vim.api.nvim_get_current_buf())
-
+      local curbufIndex = getBufIndex(api.nvim_get_current_buf())
       if not curbufIndex then
         vim.cmd("b" .. vim.t.bufs[1])
         return
       end
-
       vim.cmd(curbufIndex == 1 and "b" .. bufs[#bufs] or "b" .. bufs[curbufIndex - 1])
     end,
   },
